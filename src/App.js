9d7cdbecd4ff42356acbbe78c7ac2444f4efbf40
evsem <frukwork@gmail.com>
2022-03-11 13:22:52 +0300

    v_2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Style/App.css'
 
 const App = () => {
+  let [posts, setPosts] = useState([
+    { id: 1, title: 'JS', body: 'Exporto cuano' },
+    { id: 2, title: 'Golang', body: 'Loutao suo' },
+    { id: 3, title: 'Ruby', body: 'Sterru' },
+    { id: 4, title: 'Java', body: 'Buent helloga' },
+    { id: 5, title: 'PHP', body: 'Exporto vertia' },
+    { id: 6, title: 'Kotlin', body: 'Sercumo berrado helloga' },
+  ])
+  let [filter, setFilter] = useState({ sort: '', query: '' })
+
+  const removePost = (post) => {
+    setPosts(posts.filter((p) => p.id !== post.id))
+  }
+  const addNewPost = (newPost) => {
+    setPosts([...posts, newPost])
+  }
   return <div className="App"></div>
 }
 
